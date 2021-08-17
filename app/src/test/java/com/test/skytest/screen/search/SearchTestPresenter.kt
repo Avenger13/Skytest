3eac5d2f64efd07bd40bdaf15f43f6e25ececa6e
Muhammad13 <isit13@yandex.ru>
2021-08-17 22:04:55 +0300

    added more tests

--- app/src/test/java/com/test/skytest/screen/search/SearchTestPresenter.kt
+++ app/src/test/java/com/test/skytest/screen/search/SearchTestPresenter.kt
@@ -4,36 +4,29 @@ import com.test.skytest.data.network.api.search.response.Meaning
 import com.test.skytest.data.network.api.search.response.Translation
 import com.test.skytest.data.network.api.search.response.Word
 import com.test.skytest.data.repository.WordsRepository
-import io.mockk.mockk
-import io.mockk.mockkClass
-import io.mockk.verify
 import io.reactivex.Single
 import io.reactivex.android.plugins.RxAndroidPlugins
-import io.reactivex.observers.TestObserver
 import io.reactivex.plugins.RxJavaPlugins
-import io.reactivex.schedulers.Schedulers
 import io.reactivex.schedulers.TestScheduler
 import org.junit.After
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
+import java.util.concurrent.TimeUnit
 
 @RunWith(MockitoJUnitRunner::class)
 class SearchTestPresenter {
     @Mock
     private lateinit var view: SearchView
-  @Mock
-    private lateinit var viewState: `SearchView$$State`
-
 
     @Mock
     private lateinit var wordsRepository: WordsRepository
     private val scheduler = TestScheduler()
+    private lateinit var presenter: SearchPresenter
     val word = Word(
         10, "text", listOf(
             Meaning(
@@ -57,30 +50,62 @@ class SearchTestPresenter {
         )
     )
 
+    val words = listOf(
+        Word(1, "t1", emptyList()),
+        Word(2, "t2", emptyList()),
+    )
+
     @Before
     fun setup() {
         RxJavaPlugins.setIoSchedulerHandler { scheduler }
-        RxAndroidPlugins.setInitMainThreadSchedulerHandler {
-            scheduler
-        }
+        RxJavaPlugins.setComputationSchedulerHandler { scheduler }
+        RxJavaPlugins.setNewThreadSchedulerHandler { scheduler }
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { scheduler }
+        MockitoAnnotations.openMocks(this)
+        presenter = SearchPresenter(wordsRepository)
+        presenter.attachView(view)
+        scheduler.triggerActions()
     }
-
 
 
     @Test
-    fun clickOnWordShowsMeaningsScreen() {
-        val presenter = SearchPresenter(wordsRepository)
-presenter.attachView(view)
+    fun clickOnWord_showsMeaningsScreen() {
 
         presenter.onWordClick(word)
 
-        Mockito.verify(view).showMeanings(longArrayOf(1,2))
+        Mockito.verify(view).showMeanings(longArrayOf(1, 2))
     }
+
+
+    @Test
+    fun onSearch_withNullQuery_showsEmptyList() {
+
+        presenter.onSearch(null)
+
+        Mockito.verify(view).showSearchResults(emptyList())
+    }
+
+
+    @Test
+    fun onSearch_withNotNullQuery_showsLoadingAndResults() {
+        val query = "some query"
+        Mockito.`when`(wordsRepository.search(query)).thenReturn(Single.just(words))
+
+        presenter.onSearch(query)
+        scheduler.triggerActions()
+        scheduler.advanceTimeBy(5, TimeUnit.SECONDS)
+
+        Mockito.verify(view, Mockito.times(1)).showProgress(true)
+        Mockito.verify(view, Mockito.times(1)).showProgress(false)
+        Mockito.verify(view).showSearchResults(words)
+    }
+
 
     @After
     fun reset() {
         RxJavaPlugins.reset()
         RxAndroidPlugins.reset()
     }
+
 
 }
